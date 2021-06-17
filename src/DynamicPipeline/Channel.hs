d97a9cf1fa5092c7ee232fc8b9007e0a217f5626
Juan Pablo Royo Sales <juanpablo.royo@gmail.com>
2021-06-17 19:27:38 +0200

    Curating package

--- src/DynamicPipeline/Channel.hs
+++ src/DynamicPipeline/Channel.hs
@@ -9,28 +9,29 @@
 -- Stability   : experimental
 -- Portability : GHC
 --
-module DynamicPipeline.Channel 
-  ( ReadChannel,
-    WriteChannel,
-    DynamicPipeline.Channel.foldM,
-    foldM',
-    push,
-    pull,
-    unfoldM,
-    unfoldFile,
-    unfoldT, 
-    newChannel,
-    end
+module DynamicPipeline.Channel
+  ( ReadChannel
+  , WriteChannel
+  , DynamicPipeline.Channel.foldM
+  , foldM'
+  , push
+  , pull
+  , unfoldM
+  , unfoldFile
+  , unfoldT
+  , newChannel
+  , end
   ) where
 
-import qualified Control.Concurrent                       as CC
+import qualified Control.Concurrent                                as CC
 import           Control.Concurrent.Chan.Unagi.NoBlocking
-import           Control.Lens                             hiding ((<|))
-import           Data.ByteString                          as B
-import           Data.Comp.Algebra                        (CoalgM, anaM)
-import           Data.Foldable                            as F
+import           Control.Lens                                                                                  hiding ( (<|)
+                                                                                                                      )
+import           Data.ByteString                                   as B
+import           Data.Foldable                                     as F
 import           Data.HList
-import           Relude                                   as R
+import           GHC.IO.Handle                                     as H
+import           Relude                                            as R
 
 
 -- | 'WriteChannel' can only write values into some Channel Queue
@@ -45,7 +46,7 @@ newtype ReadChannel a = ReadChannel { unRead :: OutChan (Maybe a) }
 
 -- | 'foldM' is a /Catamorphism/ for consuming a 'ReadChannel' and do some Monadic @m@ computation with each element
 {-# INLINE foldM #-}
-foldM :: MonadIO m 
+foldM :: MonadIO m
       => ReadChannel a -- ^'ReadChannel'
       -> (a -> m ()) -- ^Computation to do with read element
       -> m ()
@@ -53,15 +54,12 @@ foldM = flip foldM' (pure ())
 
 -- | Idem 'foldM' but allows pass a monadic computation to perform at the end of the Channel
 {-# INLINE foldM' #-}
-foldM' :: MonadIO m 
+foldM' :: MonadIO m
        => ReadChannel a -- ^'ReadChannel'
        -> m () -- ^Computation to do at the end of the channel
        -> (a -> m ()) -- ^Computation to do with read element
        -> m ()
-foldM' = loop'
-  where
-    loop' c onNothing io = 
-      maybe onNothing (\e -> io e >> loop' c onNothing io) =<< liftIO (pull c)
+foldM' = loop' where loop' c onNothing io = maybe onNothing (\e -> io e >> loop' c onNothing io) =<< liftIO (pull c)
 
 -- | Push element @a@ into 'WriteChannel'
 {-# INLINE push #-}
@@ -79,44 +77,28 @@ pull = liftIO . readChan (CC.threadDelay 100) . unRead
 --
 -- [@a@]: Element get from some Source and to be write in some Channel
 --
-{-# WARNING SourceFeedCoalgM "INTERNAL USE" #-}
-data SourceFeedCoalgM m a = Done -- ^ Termination 'Term' of the Coalgebra
-                          | Computation -- ^ Continuation 'Term' of the Coalgebra
-                             { _cSeed   :: m a -- ^ Computation that Seeds the /Anamorphism/ 
-                             , _cStop   :: m Bool -- ^ Stop signal
-                             , _cOnElem :: a -> m () -- ^ Computation on Read Element
-                             }
-
--- | 'SourceFeedCoalgM' in terms of 'CoalgM' 
-{-# WARNING sourceFeedCoalgM "INTERNAL USE" #-}
-sourceFeedCoalgM :: MonadIO m => CoalgM m Maybe (SourceFeedCoalgM m a)
-sourceFeedCoalgM Done = return Nothing
-sourceFeedCoalgM c@Computation{..} = ifM _cStop 
-                                          (return $ Just Done) 
-                                          ( _cSeed >>= _cOnElem >> return (Just c) )
-
 -- | unfold from a Monadic seed @m a@ to a 'WriteChannel'
 {-# INLINE unfoldM #-}
-unfoldM :: forall m a b. MonadIO m 
+unfoldM :: forall m a b
+         . MonadIO m
         => m a -- ^Monadic Seed 
         -> (a -> b) -- ^Map input from seed to something to be written in Channel
         -> m Bool -- ^When stop unfolding
         -> WriteChannel b -- ^'WriteChannel' to write input seed elements
         -> m ()
-unfoldM seed fn stopIfM writeChannel  =
-  let onElem = flip push writeChannel . fn
-   in anaM sourceFeedCoalgM (Computation seed stopIfM onElem) >> pure ()
-
+unfoldM = loop'
+ where
+  loop' seed fn stopIfM writeChannel =
+    ifM stopIfM (pure ()) (seed >>= flip push writeChannel . fn >> loop' seed fn stopIfM writeChannel)
 -- | Using 'unfoldM', unfold from file
 {-# INLINE unfoldFile #-}
-unfoldFile :: MonadIO m 
+unfoldFile :: MonadIO m
            => FilePath -- ^Seed 'FilePath' to read from
            -> WriteChannel b -- ^'WriteChannel' to write File contents
            -> (ByteString -> b) -- ^Transform 'ByteString' read from File to something meaningful for your App
            -> m ()
-unfoldFile file writeChannel fn = liftIO $
-    R.withFile file ReadMode $ \h ->
-      unfoldM (B.hGetLine h) fn (R.hIsEOF h) writeChannel
+unfoldFile file writeChannel fn =
+  liftIO $ R.withFile file ReadMode $ \h -> unfoldM (B.hGetLine h) fn (H.hIsEOF h) writeChannel
 
 -- | Idem 'unfoldM' but for 'Foldable', for example a List @[a]@. Useful for testing purpose
 {-# INLINE unfoldT #-}
@@ -125,7 +107,7 @@ unfoldT ts writeChannel fn = forM_ ts (flip push writeChannel . fn)
 
 {-# WARNING newChannel "INTERNAL USE" #-}
 {-# NOINLINE newChannel #-}
-newChannel :: forall a. IO (WriteChannel a, ReadChannel a)
+newChannel :: forall a . IO (WriteChannel a, ReadChannel a)
 newChannel = bimap WriteChannel ReadChannel <$> newChan
 
 {-# WARNING end "INTERNAL USE" #-}
